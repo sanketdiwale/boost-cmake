1618570fee4bf067e092d369b0150483a0abf8b9
sanketdiwale <sanketdiwale@gmail.com>
2023-02-11 17:02:43 -0500

    Update date_time.cmake

--- libs/date_time.cmake
+++ libs/date_time.cmake
@@ -1,9 +1,11 @@
 _add_boost_lib(
   NAME date_time
   SOURCES
-    # ${BOOST_SOURCE}/libs/date_time/src/gregorian/greg_month.cpp
-    # ${BOOST_SOURCE}/libs/date_time/src/gregorian/greg_weekday.cpp
-    # ${BOOST_SOURCE}/libs/date_time/src/gregorian/date_generators.cpp
+    ${BOOST_SOURCE}/libs/date_time/src/gregorian/greg_names.hpp
+    ${BOOST_SOURCE}/libs/date_time/src/gregorian/gregorian_types.cpp
+    ${BOOST_SOURCE}/libs/date_time/src/gregorian/greg_month.cpp
+    ${BOOST_SOURCE}/libs/date_time/src/gregorian/greg_weekday.cpp
+    ${BOOST_SOURCE}/libs/date_time/src/gregorian/date_generators.cpp
   DEFINE_PRIVATE
     BOOST_DATE_TIME_STATIC_LINK
     DATE_TIME_INLINE
